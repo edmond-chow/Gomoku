de1da258ce39d706414c0a8793d14530ad53709b
Edmond Chow <45330596+edmond-chow@users.noreply.github.com>
2025-04-20 17:45:08 +0800

    Add files via upload

--- C#/Gomoku/Gomoku/MainForm.cs
+++ C#/Gomoku/Gomoku/MainForm.cs
@@ -1,11 +1,11 @@
 ﻿/*
  *   Gomoku
  *   
- *   A game when you are in the first role, black chesses are forbidden to
- *   win from three-three, four-four, and six or more in a row. In every
- *   single suspected survive three row, we may consider any reference points
- *   forbidden by any subsequents with three-three, four-four, five in a row,
- *   or six or more in a row.
+ *   A game when you are in the first role, Black Chesses are Forbidden to
+ *   Win from three-three, four-four, and six or more in a row. In every
+ *   single Suspected Survive Three row, we shall consider any reference
+ *   point forbidden by any subsequent with three-three, four-four, five in a
+ *   row, or six or more in a row.
  *   
  *   Copyright (C) 2022  Edmond Chow
  *   

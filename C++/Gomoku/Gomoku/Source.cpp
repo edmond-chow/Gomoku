a1eeffb9033241f5263e046608997a7b47c193b7
Edmond Chow <45330596+edmond-chow@users.noreply.github.com>
2025-04-20 18:07:22 +0800

    Add files via upload

--- C++/Gomoku/Gomoku/Source.cpp
+++ C++/Gomoku/Gomoku/Source.cpp
@@ -1,29 +1,31 @@
-﻿/*
+﻿#pragma region license
+/*
  *   Gomoku
- *
- *   A game when you are in the first role, black chesses are forbidden to
- *   win from three-three, four-four, and six or more in a row. In every
- *   single suspected survive three row, we may consider any reference points
- *   forbidden by any subsequents with three-three, four-four, five in a row,
- *   or six or more in a row.
- *
+ *   
+ *   A game when you are in the first role, Black Chesses are Forbidden to
+ *   Win from three-three, four-four, and six or more in a row. In every
+ *   single Suspected Survive Three row, we shall consider any reference
+ *   point Forbidden by any subsequent with three-three, four-four, five in a
+ *   row, or six or more in a row.
+ *   
  *   Copyright (C) 2022  Edmond Chow
- *
+ *   
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU Affero General Public License as published
  *   by the Free Software Foundation, either version 3 of the License, or
  *   (at your option) any later version.
- *
+ *   
  *   This program is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *   GNU Affero General Public License for more details.
- *
+ *   
  *   You should have received a copy of the GNU Affero General Public License
  *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *
+ *   
  *   If you have any inquiry, feel free to contact <edmond-chow@outlook.com>.
  */
+#pragma endregion
 #include <cmath>
 #include <string>
 #include <Windows.h>
